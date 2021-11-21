fef0b772831d84476476f128c0a27bcf6a0f47a9
truls <trulsmoholt@hotmail.com>
2021-11-21 20:20:16 +0100

    klar for levering

--- elliptic_convergence.py
+++ elliptic_convergence.py
@@ -16,6 +16,9 @@ from discretization.mesh import Mesh
 
 from utils.flux_error import compute_flux_error
 from utils.differentiation import divergence, gradient
+"""
+The code used for the figures in section 5.2
+"""
 
 def compute_error(mesh,u,u_fabric):
     cx = mesh.cell_centers.shape[1]
@@ -46,19 +49,12 @@ source = -divergence(gradient(u_fabric,[x,y]),[x,y],permability_tensor=K)
 source = sym.lambdify([x,y],source)
 u_lam = sym.lambdify([x,y],u_fabric)
 
-nx = 5
-ny = 5
-# T = lambda p: np.array([-0.5*p[1]+p[0],p[1]])
-T = lambda p: np.array([p[0]-0.5*p[1],p[1]])
-mesh = Mesh(20,40,chevron_perturbation(40),ghostboundary=True)
-mesh.plot()
-
 
 
 
 
 start = 3
-end = 7
+end = 5
 aspect = 1
 result_pressure = np.zeros((end-start,9))
 result_flux = np.zeros((end-start,9))
@@ -66,9 +62,9 @@ result_flux = np.zeros((end-start,9))
 for i in range(start,end):
     result_pressure[i-start,0] = i
     result_flux[i-start,0] = i
-    # mesh = Mesh(2**i,aspect*2**i,random_perturbation(1/(4*2**i),aspect),ghostboundary=True)
+    mesh = Mesh(2**i,aspect*2**i,random_perturbation(1/(4*2**i),aspect),ghostboundary=True)
     # mesh = Mesh(2**i,2*2**i,chevron_perturbation(2*2**i),ghostboundary=True)
-    mesh = Mesh(2**i,2**i,T)
+    #mesh = Mesh(2**i,2**i,T)
     num_unknowns = mesh.num_unknowns
 
 
@@ -174,7 +170,7 @@ ax2.set_title('$max$ error')
 ax2.set(xlabel='$log_2 n$',ylabel='$log_2 e$')
 plt.legend(loc='lower center',bbox_to_anchor=(0.0, -0.3),ncol=4)
 fig.subplots_adjust(bottom=0.20)
-plt.savefig('figs/pressure_chevron_grid.pdf')
+#plt.savefig('figs/pressure_chevron_grid.pdf')
 
 plt.show()
 
@@ -207,6 +203,6 @@ ax2.set_title('$max$ error')
 ax2.set(xlabel='$log_2 n$',ylabel='$log_2 e$')
 plt.legend(loc='lower center',bbox_to_anchor=(0.0, -0.3),ncol=4)
 fig.subplots_adjust(bottom=0.20)
-plt.savefig('figs/flow_chevron_grid.pdf')
+#plt.savefig('figs/flow_chevron_grid.pdf')
 
 plt.show()
